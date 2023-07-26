7d91dcebf3fe6cff52fa8f3c091f23481e0e66a5
Avishek Thapa <avishek.thapa@mindfiresolutions.com>
2023-07-26 22:41:31 +0530

    Implement creation of duplicate cabin

--- src/features/cabins/hooks/useCreateCabin.js
+++ src/features/cabins/hooks/useCreateCabin.js
@@ -2,7 +2,7 @@ import { useMutation } from '@tanstack/react-query';
 import { createCabin } from '../../../services/apiCabins';
 import useCabinsInvalidationQueries from './useCabinsInvalidationQueries';
 
-function useCreateCabin({ onSuccessHandler, onErrorHandler }) {
+function useCreateCabin({ onSuccessHandler, onErrorHandler } = {}) {
   const { invalidate } = useCabinsInvalidationQueries();
   return useMutation({
     mutationKey: ['createCabin'],
