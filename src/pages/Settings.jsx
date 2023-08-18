c94170ca4e3d808650539f0324e274233da259f8
Avishek Thapa <avishek.thapa@mindfiresolutions.com>
2023-08-18 14:59:26 +0530

    Add fetch and update of settings

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,5 +1,19 @@
+import Column from '../ui/Column';
+import Heading from '../ui/Heading';
+import UpdateSettingsForm from '../features/settings/components/UpdateSettingsForm';
+import { css } from 'styled-components';
+
 function Settings() {
-  return <p>Settings</p>;
+  return (
+    <Column
+      columnStyles={css`
+        gap: 2.5rem;
+      `}
+    >
+      <Heading as='h1'>update hotel settings</Heading>
+      <UpdateSettingsForm />
+    </Column>
+  );
 }
 
 export default Settings;
--- src/ui/Column.jsx
+++ src/ui/Column.jsx
@@ -1,8 +1,9 @@
-import { styled } from "styled-components";
+import { styled } from 'styled-components';
 
 const Column = styled.div`
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
+  ${(props) => props.columnStyles}
 `;
 
 export default Column;
